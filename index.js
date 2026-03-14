1eecbe49dbfea1ea9e2118e31ea8e8888eed3506
synendra <sarpasynendra999@gmail.com>
2026-03-14 15:51:36 +0530

    implemented password copy feature

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ passwordCopier.innerHTML = "\u2750";
 buttonGenerate.addEventListener("click", () => {
     randomPasswordGenerator();
 });
-
+function showAlertMessage() {
+    alertMessage.classList.remove("active");
+}
+function hideAlertMessage() {
+    alertMessage.classList.add("active");
+}
 function randomPasswordGenerator() {
     let password = "";
     let passwordLength = 14;
@@ -17,4 +22,16 @@ function randomPasswordGenerator() {
         password += chars.substring(randomNumber, randomNumber + 1);
     }
     inputRandomPassword.value = password;
+    hideAlertMessage();
 }
+passwordCopier.addEventListener("click", () => {
+    inputRandomPassword.select();
+    inputRandomPassword.setSelectionRange(0, 9999);
+    navigator.clipboard.writeText(inputRandomPassword.value);
+    if (inputRandomPassword.value) showAlertMessage();
+    setTimeout(() => {
+        hideAlertMessage();
+    }, 3000);
+});
+
+function copyPasswordToClipboard() {}
